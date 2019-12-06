63b751c7f247c1c49ce0d54943455caf69f21b6b
aumeisters <54903608+aumeisters@users.noreply.github.com>
2019-12-07 00:16:35 +0300

    Update script.js
    new ver

--- script.js
+++ script.js
@@ -1,55 +1,78 @@
-﻿
-  window.addEventListener('load',  async e => {
+window.addEventListener('load', async e => {
     const URL = 'https://kodaktor.ru/cart_data.json';
     const x = await fetch(URL).then(x => x.json());
-	//prices  -  x.hdd x.sdd x.usbdrive
-  	document.querySelector('#hddPrice').innerHTML = x.hdd;
+    //prices  -  x.hdd x.sdd x.usbdrive
+    document.querySelector('#hddPrice').innerHTML = x.hdd;
     document.querySelector('#ssdPrice').innerHTML = x.sdd;
     document.querySelector('#usbPrice').innerHTML = x.usbdrive;
-  	document.querySelector('#accept').addEventListener('click', function() {
-        let maxVal = document.querySelector('#maxVal').value;
+    isLimit = confirm('Если у Вас есть денежный лимит, нажмите "Ок", если лимита нет, нажмите "Отмена"', '');
+    if (isLimit === true) {
+        // if maxLimit -on;
+        document.querySelector('#accept').addEventListener('click', function() {
+            let maxVal = document.querySelector('#maxVal').value;
+            let totalCost = 0;
+            // HDD
+            document.querySelector('#item1').addEventListener('click', function() {
+                totalCost += +x.hdd;
+                if (totalCost < maxVal) {
+                    document.querySelector('#cart').innerHTML += '<p>HDD</p>';
+                    document.querySelector('#totalCost').innerHTML = totalCost;
+                } else {
+                    alert('Вы привысили запланированный максимум');
+                    totalCost -= +x.hdd;
+                };
+            });
+            //SSD
+            document.querySelector('#item2').addEventListener('click', function() {
+                totalCost += +x.sdd;
+                if (totalCost < maxVal) {
+                    document.querySelector('#cart').innerHTML += '<p>SSD</p>';
+                    document.querySelector('#totalCost').innerHTML = totalCost;
+                } else {
+                    alert('Вы привысили запланированный максимум');
+                    totalCost -= +x.sdd;
+                };
+            });
+            //USB
+            document.querySelector('#item3').addEventListener('click', function() {
+                totalCost += +x.usbdrive;
+                if (totalCost < maxVal) {
+                    document.querySelector('#cart').innerHTML += '<p>USB Drive</p>';
+                    document.querySelector('#totalCost').innerHTML = totalCost;
+                } else {
+                    alert('Вы привысили запланированный максимум');
+                    totalCost -= +x.usbdrive;
+                };
+            });
+        });
 
-        //cart start
+        // if maxLimit-off
+    } else {
+        document.querySelector('#limit').style.display = 'none';
         let totalCost = 0;
         // HDD
         document.querySelector('#item1').addEventListener('click', function() {
-          totalCost += +x.hdd;
-          if (totalCost < maxVal) {
+            totalCost += +x.hdd;
             document.querySelector('#cart').innerHTML += '<p>HDD</p>';
             document.querySelector('#totalCost').innerHTML = totalCost;
-          } else {
-            alert('Вы привысили запланированный максимум');
-            totalCost -= +x.hdd;
-          };
         });
         //SSD
         document.querySelector('#item2').addEventListener('click', function() {
-          totalCost += +x.sdd;
-          if (totalCost < maxVal) {
+            totalCost += +x.sdd;
             document.querySelector('#cart').innerHTML += '<p>SSD</p>';
             document.querySelector('#totalCost').innerHTML = totalCost;
-          } else {
-            alert('Вы привысили запланированный максимум');
-            totalCost -= +x.sdd;
-          };
         });
         //USB
         document.querySelector('#item3').addEventListener('click', function() {
-          totalCost += +x.usbdrive; 
-          if (totalCost < maxVal) {
+            totalCost += +x.usbdrive;
             document.querySelector('#cart').innerHTML += '<p>USB Drive</p>';
             document.querySelector('#totalCost').innerHTML = totalCost;
-          } else {
-            alert('Вы привысили запланированный максимум');
-            totalCost -= +x.usbdrive; 
-          };
         });
-	});
-  //cart end
-  //reload option
+    };
+    //reload option
     document.querySelector('#reload').addEventListener('click', function() {
-      window.location.reload(false);
+        window.location.reload(false);
     });
-  });
+});
   
  
