6ba29997e37bbd957c16d45059a1f5a816507356
aumeisters <54903608+aumeisters@users.noreply.github.com>
2019-12-10 18:35:52 +0300

    drag option -on

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ window.addEventListener('load', async e => {
     isLimit = confirm('Если у Вас есть денежный лимит, нажмите "Ок", если лимита нет, нажмите "Отмена"', '');
     if (isLimit === true) {
         // if maxLimit -on;
+        // onclick
         document.querySelector('#accept').addEventListener('click', function() {
             let maxVal = document.querySelector('#maxVal').value;
             let totalCost = 0;
@@ -44,7 +45,60 @@ window.addEventListener('load', async e => {
                     totalCost -= +x.usbdrive;
                 };
             });
+            //ondrag with limit
+            const el1 = document.querySelector('#item1'),
+                el2 = document.querySelector('#item2'),
+                el3 = document.querySelector('#item3'),
+                d = document.querySelector('#droppable');
+            el1.addEventListener('dragstart', ev => ev.dataTransfer.setData('text/plain', ev.target.id));
+            el2.addEventListener('dragstart', ev => ev.dataTransfer.setData('text/plain', ev.target.id));
+            el3.addEventListener('dragstart', ev => ev.dataTransfer.setData('text/plain', ev.target.id));
+            d.addEventListener('dragover', ev => ev.preventDefault()); // позволить продолжить бросание  
+            d.addEventListener('drop', function(e) {
+                if (e.dataTransfer.getData('text/plain') == 'item1') {
+                    totalCost += +x.hdd;
+                    if (totalCost < maxVal) {
+                        document.querySelector('#cart').innerHTML += '<p>HDD</p>';
+                        document.querySelector('#totalCost').innerHTML = totalCost;
+                        document.querySelector('#droppable').style.background = 'rgba(100,227,126,0.5)';
+                        setTimeout(function() {
+                            document.querySelector('#droppable').style.background = "URL('https://cdn2.iconfinder.com/data/icons/marmalade/128/128.png')";
+                        }, 200);
+                    } else {
+                        alert('Вы привысили запланированный максимум');
+                        totalCost -= +x.hdd;
+                    };
+                } else if (e.dataTransfer.getData('text/plain') == 'item2') {
+                    totalCost += +x.sdd;
+                    if (totalCost < maxVal) {
+                        document.querySelector('#cart').innerHTML += '<p>SSD</p>';
+                        document.querySelector('#totalCost').innerHTML = totalCost;
+                        document.querySelector('#droppable').style.background = 'rgba(100,227,126,0.5)';
+                        setTimeout(function() {
+                            document.querySelector('#droppable').style.background = "URL('https://cdn2.iconfinder.com/data/icons/marmalade/128/128.png')";
+                        }, 200);
+                    } else {
+                        alert('Вы привысили запланированный максимум');
+                        totalCost -= +x.sdd;
+                    };
+                } else if (e.dataTransfer.getData('text/plain') == 'item3') {
+                    totalCost += +x.usbdrive;
+                    if (totalCost < maxVal) {
+                        document.querySelector('#cart').innerHTML += '<p>USB Drive</p>';
+                        document.querySelector('#totalCost').innerHTML = totalCost;
+                        document.querySelector('#droppable').style.background = 'rgba(100,227,126,0.5)';
+                        setTimeout(function() {
+                            document.querySelector('#droppable').style.background = "URL('https://cdn2.iconfinder.com/data/icons/marmalade/128/128.png')";
+                        }, 200);
+                    } else {
+                        alert('Вы привысили запланированный максимум');
+                        totalCost -= +x.usbdrive;
+                    };
+                };
+
+            });
         });
+
 
         // if maxLimit-off
     } else {
@@ -68,11 +122,41 @@ window.addEventListener('load', async e => {
             document.querySelector('#cart').innerHTML += '<p>USB Drive</p>';
             document.querySelector('#totalCost').innerHTML = totalCost;
         });
+        //ondrag with limit off
+        const el1 = document.querySelector('#item1'),
+            el2 = document.querySelector('#item2'),
+            el3 = document.querySelector('#item3'),
+            d = document.querySelector('#droppable');
+        el1.addEventListener('dragstart', ev => ev.dataTransfer.setData('text/plain', ev.target.id));
+        el2.addEventListener('dragstart', ev => ev.dataTransfer.setData('text/plain', ev.target.id));
+        el3.addEventListener('dragstart', ev => ev.dataTransfer.setData('text/plain', ev.target.id));
+        d.addEventListener('dragover', ev => ev.preventDefault()); // позволить продолжить бросание  
+        d.addEventListener('drop', function(e) {
+            if (e.dataTransfer.getData('text/plain') == 'item1') {
+                totalCost += +x.hdd;
+                document.querySelector('#cart').innerHTML += '<p>HDD</p>';
+                document.querySelector('#totalCost').innerHTML = totalCost;
+                document.querySelector('#droppable').style.background = 'rgba(100,227,126,0.5)';
+                setTimeout(function() {
+                    document.querySelector('#droppable').style.background = "URL('https://cdn2.iconfinder.com/data/icons/marmalade/128/128.png')";
+                }, 200);
+            } else if (e.dataTransfer.getData('text/plain') == 'item2') {
+                totalCost += +x.sdd;
+                document.querySelector('#cart').innerHTML += '<p>SSD</p>';
+                document.querySelector('#totalCost').innerHTML = totalCost;
+                document.querySelector('#droppable').style.background = 'rgba(100,227,126,0.5)';
+                setTimeout(function() {
+                    document.querySelector('#droppable').style.background = "URL('https://cdn2.iconfinder.com/data/icons/marmalade/128/128.png')";
+                }, 200);
+            } else if (e.dataTransfer.getData('text/plain') == 'item3') {
+                totalCost += +x.usbdrive;
+                document.querySelector('#cart').innerHTML += '<p>USB Drive</p>';
+                document.querySelector('#totalCost').innerHTML = totalCost;
+                document.querySelector('#droppable').style.background = 'rgba(100,227,126,0.5)';
+                setTimeout(function() {
+                    document.querySelector('#droppable').style.background = "URL('https://cdn2.iconfinder.com/data/icons/marmalade/128/128.png')";
+                }, 200);
+            };
+        });
     };
-    //reload option
-    document.querySelector('#reload').addEventListener('click', function() {
-        window.location.reload(false);
-    });
 });
-  
- 
